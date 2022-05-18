5e7509d7404f190a7a205f08dd31801c01cbc491
jasvan <javeriano33@hotmail.com>
2022-05-18 16:08:15 -0500

    actualizando tabla

--- resources/views/favoritos/favoritos.blade.php
+++ resources/views/favoritos/favoritos.blade.php
@@ -4,66 +4,59 @@
 
 @section('content')
 
-    <div class='content'>
+    <div class='content1'>
         <div class="titulo">
             <h1>Tu lista de favoritos</h1>
         </div>
-        <div class="table">
-            <div class='thead'>
-                <div class='tr'>
-                    <div id="url1" class='th'>Titulo</div>
-                    <div id="url2" class='th'>Url</div>
-                    <div id="url3" class='th'>Editar</div>
-                    <div id="url5" class='th opcion'>Opcion</div>
-                </div>
+
+        <form method="" action="">
+            <div class="contenedor">
+                <table>
+                    <div class="contenido">
+                        <thead id="head">
+                            <tr>
+                                <th id="th1">TITULO</th>
+                                <th id="th2">URL</th>
+                                <th id="th3">DESCRIPCIÓN</th>
+                                <th id="th4">CATEGOÍA</th>
+                                <th id="th5">EDITAR</th>
+                                <th id="th6">OPCIÓN</th>
+                            </tr>
+                        </thead>
+                    </div>
+                    <tbody>
+                        @foreach ($datos as $favoritos)
+                            <tr id="color">
+                                <td id="td1"> {{ $favoritos->titulo }} </td>
+                                <td id="td2">
+                                    <a href="">
+                                        {{ $favoritos->url }}
+                                    </a>
+                                </td>
+                                <td id="td3"> {{ $favoritos->categoria }}</td>
+                                <td id="td4"> {{ $favoritos->descripcion }}</td>
+                                <td id="td5">
+                                    <div class='btntd penzil'>
+                                    </div>
+                                </td>
+                                <td id="td6">
+                                    <div >
+                                        <button class='btntd trash'></button>
+                                        <input type="hidden" name=" {{ $favoritos->id }} " />
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+
+                </table>
             </div>
-            <form action="" method="POST">
-                <div class='tbody'>
-
-                    @foreach ($datos as $favoritos)
-                        <div class='tr odd'>
-
-                            <div id="td1">
-                                {{ $favoritos->titulo }}
-                            </div>
-                        </div>
-                        <div class='tr even'>
-
-                            <div id="td2">
-                                <a href="">
-                                    {{ $favoritos->url }}
-                                </a>
-                            </div>
-                        </div>
-                        <div class='tr odd'>
-                            <div id="td3">
-                                <div class='btntd penzil'>
-                                </div>
-                            </div>
-                        </div>
-                        <div class='tr even'>
-                            <div id="td5">
-                                <div class='btntd trash'>
-                                    <input type="hidden" name=" {{ $favoritos->id }} " />
-                                </div>
-                            </div>
-
-                        </div>
-                    @endforeach
-
-                </div>
-                <div class="editar">
-                    <input type="hidden" id="edit">
-                </div>
-            </form>
-
-        </div>
-        <div class='tfoot'>
+        </form>
+        <tfoot>
             <div class='section-pagina'>
                 <hr />
                 <div class='paginate'> Total paginas <strong>{{ $datos->render() }}</strong></div>
-            </div>
-        </div>
+            </div>  
     </div>
 
 @endsection
