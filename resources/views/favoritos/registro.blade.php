c3dc6f313fe36b2a35189dea4040096be86174b2
jasvan <javeriano33@hotmail.com>
2022-05-18 20:48:37 -0500

    fix

--- resources/views/favoritos/registro.blade.php
+++ resources/views/favoritos/registro.blade.php
@@ -23,7 +23,7 @@
 
                 <label for="contrasena2">Confirme su contraseña
                     <input class="input" type="password2" name="password2" />
-                </label> --}}
+                </label>
 
                 <input type="submit" name="" id="submit" value="Registrar" />
             </fieldset>
